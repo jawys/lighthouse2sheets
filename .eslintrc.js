480facc53e7749d2420b1c160dc672d00ce6dbf1
Wystub, Jan <jan.wystub@hsrw.org>
2018-11-05 14:15:42 +0100

    update lint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,20 @@
 module.exports = {
 	parser: 'babel-eslint',
 	parserOptions: {
-		sourceType: 'module'
+		sourceType: 'module',
 	},
 	extends: ['airbnb-base'],
 	rules: {
-		'comma-dangle': 0,
-		'no-tabs': 0,
-		indent: 0,
 		'arrow-parens': 0,
-		'no-param-reassign': 0
-	}
+		'comma-dangle': 0,
+		indent: [2, 'tab'],
+		'linebreak-style': [2, 'unix'],
+		'no-param-reassign': 0,
+		'no-tabs': 0,
+		'no-undef': 1,
+		'no-unused-vars': 1,
+		quotes: [2, 'single'],
+		semi: [2, 'always'],
+		'template-curly-spacing': [2, 'always'],
+	},
 };
